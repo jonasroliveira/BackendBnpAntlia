3a3bd0ca6ac94251f785eb2f98439f18537547b6
Jonas Oliveira <jonasroliveira@gmail.com>
2025-11-14 16:13:28 -0300

    Ok busca produto.

--- Repositories/ProdutoRepository.cs
+++ Repositories/ProdutoRepository.cs
@@ -1,3 +1,5 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
 using BackendBnpAntlia.DTOs;
 using BackendBnpAntlia.Interfaces;
 
@@ -5,8 +7,36 @@ namespace BackendBnpAntlia.Repositories;
 
 public class ProdutoRepository : IProdutoRepository
 {
-    public Task<IEnumerable<ProdutoDto>> ObterProdutos()
+    private readonly string _connectionString;
+    public ProdutoRepository(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+    public async Task<IEnumerable<ProdutoDto>> ObterProdutos()
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            var produtos = new List<ProdutoDto>();
+            using (var command = new SqlCommand("sp_ObterProdutos", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                await connection.OpenAsync();
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        produtos.Add(new ProdutoDto
+                        {
+                            COD_PRODUTO = reader["COD_PRODUTO"] == DBNull.Value ? null : Convert.ToString(reader["COD_PRODUTO"]),
+                            DES_PRODUTO = reader["DES_PRODUTO"] == DBNull.Value ? null : Convert.ToString(reader["DES_PRODUTO"]),
+                            STA_STATUS = reader["STA_STATUS"] == DBNull.Value ? null : Convert.ToString(reader["STA_STATUS"])
+                        });
+                    }
+                }
+            }
+            return produtos;
+        }
     }
 }
